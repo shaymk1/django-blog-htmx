ad8a23da35e52e7315c3ec5cca810a0b7cf07d74
unknown <mkekae@gmail.com>
2024-01-11 21:19:36 +0200

    created a secret key

--- core/settings/base_settings.py
+++ core/settings/base_settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*13$unr_ysd**$51#(d0eb+0#@xd#*=pkwo)2n%1(bysp#wxym'
+
+SECRET_KEY = 'bjcry^8j&&+z0%qxkhjwcm41eaeo=fsa3254_ztxft1bvkws=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
